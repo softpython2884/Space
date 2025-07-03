5738078e6e9f3e4cf73611f57bf636772958f091
Enzo prados <enzo.prados@gmail.com>
2025-07-03 21:53:07 +0000

    par contre, les astéroid la prend toute notre vie, le bouclier energetiq

--- src/components/game/player-ship.tsx
+++ src/components/game/player-ship.tsx
@@ -4,14 +4,23 @@ import { motion } from 'framer-motion';
 import { AimIndicator } from './aim-indicator';
 
 interface PlayerShipProps {
+  x: number;
+  y: number;
   rotation: number;
   aimRotation: number;
   isShieldActive?: boolean;
 }
 
-export function PlayerShip({ rotation, aimRotation, isShieldActive }: PlayerShipProps) {
+export function PlayerShip({ x, y, rotation, aimRotation, isShieldActive }: PlayerShipProps) {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid place-items-center w-36 h-36 z-10 pointer-events-none">
+    <div
+      className="absolute top-0 left-0 pointer-events-none"
+      style={{
+        transform: `translate(${x}px, ${y}px)`,
+        willChange: 'transform',
+      }}
+    >
+      <div className="relative -translate-x-1/2 -translate-y-1/2 grid place-items-center w-36 h-36 z-10">
         {isShieldActive && (
           <motion.div
             className="absolute row-start-1 col-start-1"
@@ -44,6 +53,7 @@ export function PlayerShip({ rotation, aimRotation, isShieldActive }: PlayerShip
             <polygon points="25,5 45,45 25,35 5,45" strokeWidth="2" />
           </svg>
         </motion.div>
+      </div>
     </div>
   );
 }
